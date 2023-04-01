050bf65d8a88cc165e7a716ef083d057a1cc62ac
SimoneS86 <simone.sensini.86@gmail.com>
2023-04-01 17:17:53 +0200

    fine

---
+++ src/components/MyMovie.jsx
@@ -0,0 +1,7 @@
+const MyMovie = (props) => (
+  <a href="#" className="slider-item me-2 flex-grow-0 flex-shrink-0">
+    <img className="img-fluid " src={props.img} />
+  </a>
+);
+
+export default MyMovie;
